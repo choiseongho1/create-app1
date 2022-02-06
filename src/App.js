47d0a711b463403598d34fbbc2dfde0f4650a2e6
SeongHo <67409492+choiseongho1@users.noreply.github.com>
2022-02-06 12:18:15 +0900

    EFFECTS_Deps

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] =useState(0);
-  const onClick = () => setValue ((prev) => prev +1);
-  console.log("i run all the time");
-  
-  useEffect(() => {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChnage = (e) => setKeyword(e.target.value);
 
-  }, []); // useEffect 를 사용하면 코드가 한번만 render
+  useEffect(() => {
+    console.log("I run only once");
+  }, []); // 배열에 값이 없기 때문에 화면 로드시 한번만 render
+
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]); // keyword가 변화할때마다 render
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]); // counter 변화할때마다 render
+
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]); // keyword & counter 변화할때마다 render
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChnage}
+        type="text"
+        placeholder="Search here "
+      />
       <h1>{counter}</h1>
-      <button onClick = {onClick}>click me</button>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
 
 export default App;
-  
