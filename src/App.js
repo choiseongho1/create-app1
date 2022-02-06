0dd779d079eab47a4059141c58381756ead99f2c
SeongHo <67409492+choiseongho1@users.noreply.github.com>
2022-02-06 12:51:52 +0900

    To Do List Part One

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("create");
-    return () => console.log("bye");
-  }, []);
-  //cleanUp function
-  //cleanUp을 사용하게 되면 컴포넌트가 언제 create되었고, destoryed되었는지 시점 확인 가능
-  //컴포넌트가 파괴될 때 hiFn이 return한 byeFn을 실행
-
-  return <h1>Hello</h1>;
-}
-
+import { useState } from "react";
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (e) => setTodo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+  console.log(todos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
+      <h1>My To-do List ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Write your to-do"
+        />
+        <button>Add To-do</button>
+      </form>
     </div>
   );
 }
